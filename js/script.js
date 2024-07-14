a8b85c40b83977e2241e4a6bb428b4dec0acd84b
thisismammad <thisismamamd7@gmail.com>
2024-07-14 18:39:58 +0330

    add sub-menu

--- js/script.js
+++ js/script.js
@@ -10,13 +10,32 @@ openMenu.addEventListener("click", () => {
 closeMenu.addEventListener("click", () => {
   if (menu) {
     menu.style.right = "-100%";
+   $(".sub-menu").slideUp(300);
   }
 });
 
+var prevWidth = window.innerWidth;
+window.addEventListener("resize", function () {
+  var width = window.innerWidth;
+  if (width !== prevWidth) {
+    prevWidth = width;
+    menu.style.right = "-100%";
+    $("aside .sub-menu").slideUp(300);
+  }
+});
 
-const goTop = document.querySelector("#top")
+const goTop = document.querySelector("#top");
 
-goTop.addEventListener("click",(e)=>{
-    e.preventDefault();
-    window.scrollTo(0, 0);
-})
+goTop.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.scrollTo(0, 0);
+});
+
+$(".has-sub").on("click", () => {
+  const sub = $(".sub-menu");
+  if(sub.css("display") =="block"){
+    sub.slideUp(300)
+  }else{
+    sub.slideDown(300)
+  }
+});
